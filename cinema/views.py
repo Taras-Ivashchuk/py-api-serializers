9014fb93ddc6bf644a3a58825b63bbe0947b6d15
Taras Ivashchuk <tvishy@disroot.org>
2025-12-31 15:36:04 +0200

    update: add annotations

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,8 @@
+from typing import Type
+
+from django.db.models import QuerySet
 from rest_framework import viewsets
+from rest_framework.serializers import Serializer
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
 from cinema.serializers import (
@@ -30,24 +34,24 @@ class CinemaHallViewSet(viewsets.ModelViewSet):
 
 
 class MovieViewSet(viewsets.ModelViewSet):
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> Type[Serializer]:
         if self.action == "list":
             return MovieListSerializer
         elif self.action in ("update", "create", "partial_update"):
             return MovieCreateUpdateSerializer
         return MovieDetailSerializer
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet[Movie]:
         return Movie.objects.prefetch_related("genres", "actors")
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
-    def get_serializer_class(self):
+    def get_serializer_class(self)-> Type[Serializer]:
         if self.action == "list":
             return MovieSessionListSerializer
         elif self.action == "retrieve":
             return MovieSessionDetailSerializer
         return MovieSessionCreateUpdateSerializer
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet[MovieSession]:
         return MovieSession.objects.select_related("movie", "cinema_hall")
